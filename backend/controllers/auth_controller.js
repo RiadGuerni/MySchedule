6d552773325df50d3c3787be7fd663c5992ef58f
RiadGuerni <riadguerni77@gmail.com>
2025-02-07 06:03:57 -1100

    manual session configuaration
    switched from passport session to manual configuration

--- backend/controllers/auth_controller.js
+++ backend/controllers/auth_controller.js
@@ -1,11 +1,29 @@
 const authService = require("../services/auth_service");
-const jwtUtils = require("../utils/jwt_utils");
+
 const passport = require("passport");
 
 const { validationResult } = require("express-validator");
 
 class AuthController {
-    static async register(req, res, next) {
+    static async register(req, res,next) {
+        
+        try {
+            const errors = validationResult(req);
+            console.log(errors);
+            if (!errors.isEmpty()) {
+                const error = new Error(errors.array()[0].msg);
+                error.statusCode = 400;
+                throw error;
+            }
+            const { email, password } = req.body;
+            const user = await authService.register({ email, password });
+            req.session.userId = user._id;
+            res.status(201).json({ message: "User registered successfully"});
+        } catch (error) {
+            next(error);
+        }
+    }
+    static async login(req, res,next) {
         try {
             const errors = validationResult(req);
             if (!errors.isEmpty()) {
@@ -13,29 +31,18 @@ class AuthController {
                 error.statusCode = 400;
                 throw error;
             }
-            
             const { email, password } = req.body;
-            const user = await authService.register({ email, password });
-            req.login(user, (err) => {
-                if (err) return next(err);
-                res.status(201).json({ 
-                  message: "User registered and logged in", 
-                  user: { email : user.email}
-                });
-              });
+            const user = await authService.login({ email, password });
+            req.session.userId = user._id;
+            res.status(200).json({ message: "User logged in successfully"});
         } catch (error) {
             next(error);
         }
     }
-
-    static async login(req, res, next) {
-        // passport handle authentication
-
-            const user = req.user;
-            
-            res.status(200).json({ message: "User logged in successfully", user : { email : user.email } });
-        
-    }
-}
+    static async (req, res)  {
+        req.session.destroy(() => {
+            res.json({ message: "Logged out" });
+        });
+    }}
 
 module.exports = AuthController;
