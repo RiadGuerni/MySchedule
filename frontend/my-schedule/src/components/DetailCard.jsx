1d715efec3618432c4f73087a47eabc6e1bd24f9
Younes M <younesmpro21@gmail.com>
2025-02-07 18:25:05 +0100

    fix problem with params in Details card

--- frontend/my-schedule/src/components/DetailCard.jsx
+++ frontend/my-schedule/src/components/DetailCard.jsx
@@ -1,9 +1,8 @@
-import React from 'react'
-
-export default function DetailCard(title, description) {
-  return (
-    <div>
-      details card
-    </div>
-  )
-}
+export default function DetailCard({ title, description }) {
+    return (
+      <div className="bg-slate-600"> 
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
+    );
+  }
--- frontend/my-schedule/src/components/details.jsx
+++ frontend/my-schedule/src/components/details.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import DetailCard from './DetailCard'
+
 
 export default function Details() {
   return (
     <div>
       <p className=''>Why MySchedule?</p>
+      <DetailCard title="Customisable" description="dsqfdsqf"/>
     </div>
   )
 }
