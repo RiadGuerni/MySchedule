06f97f56fcd715e6a5382df2d2ecfe0a34aa85e1
Younes M <younesmpro21@gmail.com>
2025-02-08 09:31:16 +0100

    small refactor and add link for login page

--- frontend/my-schedule/src/components/getStarted.jsx
+++ frontend/my-schedule/src/components/getStarted.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
+import Link from 'next/link'
 
 export default function GetStarted() {
     return (
         <div className='flex flex-col justify-center gap-8 items-center place-content-center'>
             <p className='text-3xl font-bold  mt-16 '>So what are you waiting for?</p>
-            <button className='bg-[#323030] text-white px-12 py-2 text-2xl rounded-md hover:bg-[#4e4a4a]'>Create your schedules</button>
+            <Link href='/login' className='bg-[#323030] text-white px-12 py-2 text-2xl rounded-md hover:bg-[#4e4a4a]'>Create your schedules</Link>
         </div>
-
-
-
     )
 }
 
